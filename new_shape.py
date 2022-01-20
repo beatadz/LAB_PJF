6e173fa0b2241628453ed55be5e5c0671adaaafa
beatadz <beata.dziewulska@gmail.com>
2022-01-20 17:10:29 +0100

    Zapewnienie spójność nowych kształtów v2

--- new_shape.py
+++ new_shape.py
@@ -9,6 +9,10 @@ class NewShape(object):
         self.grid = grid
         self.new_shapes_colors = [[grid.main_color for x in range(4)] for y in range(4)]
         self.colors = []
+        self.correct = True
+        self.check_r = True
+
+        self.count = 0
 
     def save_shape(self, grid, nr):
         empty = True
@@ -19,6 +23,7 @@ class NewShape(object):
                     self.new_shapes_colors[i][j] = grid.select[i][j]
                     self.colors.append(grid.select[i][j])
                     self.row = self.row + "1"
+                    self.count += 1
                     empty = False
                 else:
                     self.row = self.row + "0"
@@ -37,28 +42,54 @@ class NewShape(object):
                 shapes.colors.append(self.colors)
                 # check if there are empty spaces between blocks
                 for i in range(0, 4):
+                    self.check_r = True
                     for j in range(0, 4):
-                        if 0 < i < 3:
-                            if 0 < j < 3:
-                                if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i + 1][j] == grid.main_color and grid.select[i][j - 1] == grid.main_color and grid.select[i][j + 1] == grid.main_color:
-                                    empty = True
-                        elif i == 0:
-                            if j == 0:
-                                if grid.select[i][j] != grid.main_color and grid.select[i + 1][j] == grid.main_color and grid.select[i][j + 1] == grid.main_color:
-                                    empty = True
-                            elif j == 3:
-                                if grid.select[i][j] != grid.main_color and grid.select[i + 1][j] == grid.main_color and grid.select[i][j - 1] == grid.main_color:
-                                    empty = True
-                        elif i == 3:
-                            if j == 0:
-                                if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i][j + 1] == grid.main_color:
-                                    empty = True
-                            if j == 0:
-                                if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i][j - 1] == grid.main_color:
-                                    empty = True
+                        self.check_c = True
+                        if self.count > 1:
 
-                if empty is False:
+                            if 0 < i < 4:
+                                r = 0
+                                if 0 < j < 3:
+                                    if r > 0:
+                                        if grid.select[i][j] != grid.main_color and not (grid.select[i - 1][j] == grid.main_color
+                                                                                         and grid.select[i - 1][j - 1] == grid.main_color
+                                                                                         and grid.select[i - 1][j + 1] == grid.main_color):
+                                            self.check_r = False
+                            if 0 < i < 3:
+                                for k in range(0, i - 1):
+                                    for l in range(0, 4):
+                                        if grid.select[i][j] != grid.main_color:
+                                            r = 1
+                                if 0 < j < 3:
+                                    if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i + 1][j] == grid.main_color \
+                                            and grid.select[i][j - 1] == grid.main_color and grid.select[i][j + 1] == grid.main_color and grid.select[i + 1][j + 1] == grid.main_color\
+                                            and grid.select[i - 1][j - 1] == grid.main_color and grid.select[i - 1][j + 1] == grid.main_color and grid.select[i + 1][j - 1] == grid.main_color:
+                                        self.correct = False
+                            elif i == 0:
+                                if j == 0:
+                                    if grid.select[i][j] != grid.main_color and grid.select[i + 1][j] == grid.main_color and grid.select[i][j + 1] == grid.main_color \
+                                            and grid.select[i + 1][j + 1] == grid.main_color:
+                                        self.correct = False
+                                elif j == 3:
+                                    if grid.select[i][j] != grid.main_color and grid.select[i + 1][j] == grid.main_color and grid.select[i][j - 1] == grid.main_color \
+                                            and grid.select[i + 1][j - 1] == grid.main_color:
+                                        self.correct = False
+                            elif i == 3:
+                                if j == 0:
+                                    if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i][j + 1] == grid.main_color \
+                                            and grid.select[i - 1][j + 1] == grid.main_color:
+                                        self.correct = False
+                                if j == 3:
+                                    if grid.select[i][j] != grid.main_color and grid.select[i - 1][j] == grid.main_color and grid.select[i][j - 1] == grid.main_color \
+                                            and grid.select[i - 1][j - 1] == grid.main_color:
+                                        self.correct = False
+                        if self.check_r is True:
+                            self.correct = False
+                if empty is False and self.correct is True:
                     print("saved")
+                else:
+                    empty = True
             else:
                 empty = True
+
         return empty
